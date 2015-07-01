495826073fe785fa4a426b2dd96af8e0353abbd8
c650 <cbailey@palmertrinity.org>
2015-07-01 17:03:44 -0400

    CHANGED TO TWILIO

---
+++ models/model.rb
@@ -0,0 +1,13 @@
+require 'twilio-ruby'
+
+account_sid = 'AC633a4f798afcb2aba4ae658ab3ddf465'
+
+auth_token = 'dade8c6c18cad71adc0947977dd5edf7'
+
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+ @client.account.messages.create({
+	:from => '+17862920603',  
+	:to => '+13052818195',
+	:body => "Hey buddy!"
+})
